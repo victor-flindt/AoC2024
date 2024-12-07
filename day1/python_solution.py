f6798cfdc323e1f3965ca7b24795e5f0d2a229b5
victor-flindt <victorflindt@gmail.com>
2024-12-07 21:37:05 +0100

    python solution working

---
+++ day1/python_solution.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('data.txt', delimiter='\s+', names = ['list1','list2'])
+
+
+list1 = np.sort(df['list1'].to_numpy())
+
+
+list2 = np.sort(df['list2'].to_numpy())
+
+
+distance = [abs(cell1-cell2) for cell1,cell2 in zip(list1,list2)]
+
+print(f'answer:{sum(distance)}')

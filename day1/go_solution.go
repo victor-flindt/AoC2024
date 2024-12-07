3948e15f1950f403989ead8ebd000c971965e425
victor-flindt <victorflindt@gmail.com>
2024-12-07 23:03:05 +0100

    added golang solution

---
+++ day1/go_solution.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+)
+
+func main() {
+
+	file, err := os.Open("data.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 2
+	data, err := reader.ReadAll()
+
+	var list1 []float64
+	var list2 []float64
+
+	for _, row := range data {
+
+		if len(row) != 0 {
+			var el1, _ = strconv.ParseFloat(row[0], 32)
+			var el2, _ = strconv.ParseFloat(row[1], 32)
+			list1 = append(list1, el1)
+			list2 = append(list2, el2)
+		}
+	}
+
+	slices.Sort(list1)
+	slices.Sort(list2)
+	var total_distance float64
+	for index, _ := range list1 {
+		total_distance += math.Abs(list1[index] - list2[index])
+	}
+
+	fmt.Println(total_distance)
+
+	defer file.Close()
+
+}
--- day1/python_solution.py
+++ day1/python_solution.py
@@ -3,13 +3,9 @@ import numpy as np
 
 df = pd.read_csv('data.txt', delimiter='\s+', names = ['list1','list2'])
 
-
 list1 = np.sort(df['list1'].to_numpy())
-
-
 list2 = np.sort(df['list2'].to_numpy())
 
 
 distance = [abs(cell1-cell2) for cell1,cell2 in zip(list1,list2)]
-
 print(f'answer:{sum(distance)}')

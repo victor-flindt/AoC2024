635f0541170fd79ebab07ea50d46b72a7b80e235
victor-flindt <victorflindt@gmail.com>
2024-12-09 13:39:55 +0100

    stashing

--- day1/python_solution.py
+++ day1/python_solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('data.txt', delimiter='\s+', names = ['list1','list2'])
+df = pd.read_csv('data.txt', delimiter=',', names = ['list1','list2'])
 
 list1 = np.sort(df['list1'].to_numpy())
 list2 = np.sort(df['list2'].to_numpy())

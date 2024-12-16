cdff64b9cf8c26a6729bd21c677811b2497ad2dd
victor-flindt <victorflindt@gmail.com>
2024-12-16 10:51:17 +0100

    part2 working in golang

--- day3/go_solution.go
+++ day3/go_solution.go
@@ -21,7 +21,26 @@ func find_digits(str string) (a int64, b int64) {
 
 }
 
+func find_mul(data string) []string {
+	regex, _ := regexp.Compile("mul\\(\\d{1,3},\\d{1,3}\\)")
+
+	matches := regex.FindAllString(data, -1)
+
+	return matches
+
+}
+
 func find_dont(data string) [][]int {
+
+	regex, _ := regexp.Compile("don't\\(\\)")
+
+	matches := regex.FindAllStringIndex(data, -1)
+
+	return matches
+
+}
+
+func find_do(data string) [][]int {
 
 	regex, _ := regexp.Compile("do\\(\\)")
 
@@ -33,40 +52,51 @@ func find_dont(data string) [][]int {
 
 func main() {
 
-	bytes, err := os.ReadFile("test_data_part2.data")
+	bytes, err := os.ReadFile("data.data")
 	if err != nil {
 		panic(err)
 	}
 
 	data_as_string := string(bytes)
 
-	// donts := find_dont(data_as_string)
+	donts := find_dont(data_as_string)
+	dos := find_do(data_as_string)
 
-	fmt.Println(data_as_string[59:60], data_as_string[62:63])
+	var valid_indexes [][]int
+	valid_indexes = append(valid_indexes, []int{0, donts[0][0]})
 
-	// var total int64 = 0
+	dont := 0
+	var do int = 0
+	for _, do_array := range dos {
+		do = do_array[0]
+		if do < dont {
+			continue
+		}
+		for _, dont_array := range donts {
+			dont = dont_array[0]
+			if dont < do {
+				continue
+			}
+			if do < dont {
+				start_stop := []int{do, dont}
+				valid_indexes = append(valid_indexes, start_stop)
+				break
+			}
+		}
+		if dont < do {
+			start_stop := []int{do, len(data_as_string)}
+			valid_indexes = append(valid_indexes, start_stop)
+		}
+	}
+	var total int64 = 0
+	for _, valid_index := range valid_indexes {
 
-	// for {
-	// 	// read row in from csv file
-	// 	row, err := reader.Read()
+		results := find_mul(data_as_string[valid_index[0]:valid_index[1]])
 
-	// 	if err != nil {
-	// 		if err.Error() == "EOF" {
-	// 			break
-	// 		}
-	// 		panic(err)
-	// 	}
-
-	// 	// regex, _ := regexp.Compile("mul\\(\\d{1,3}\\,\\d{1,3}\\)")
-
-	// 	// matches := regex.FindAllString(strings.Join(row[:], ","), -1)
-	// 	// for _, match := range matches {
-	// 	// 	a, b := find_digits(match)
-	// 	// 	total += a * b
-
-	// 	// }
-	// }
-
-	// fmt.Println(total)
-
+		for _, result := range results {
+			a, b := find_digits(result)
+			total += a * b
+		}
+	}
+	fmt.Println(total)
 }
